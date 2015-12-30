be2deb87f07640587c5ee5ef1c6bb4c08cc4c707
Luke <lukebluett@mail.com>
2015-12-30 22:27:56 +0000

    modified:   Create_Password.py [Updated the constructor put in +=, added code to rand_charactor method]

--- src/passwordmanager/Create_Password.py
+++ src/passwordmanager/Create_Password.py
@@ -10,17 +10,23 @@ class Create_Password:
 
    def __init__(self, has_lowercase, has_uppercase, has_numbers, has_special):
       if(has_lowercase):
-         self.__available = self.__available + self.__lowercase
+         self.__available += self.__lowercase
       if(has_uppercase):
-         self.__available = self.__available + self.__uppercase 
+         self.__available += self.__uppercase 
       if(has_numbers):
-         self.__available = self.__available + self.__numbers
+         self.__available += self.__numbers
       if(has_special):
-         self.__available = self.__available + self.__special
+         self.__available += self.__special
 
    def random_charactors(self, length):
-      #Code to create a password of size length using random charactors from available
-      return ''
+      password = ''
+      j = 0
+      
+      for i in range (0, length):
+         j = randint(0, len(self.__available))
+         password += available[j, (j + 1)]
+      
+      return password
 
    def random_words(self, length):
       #Code to create a password of size length using random words from wordslist
