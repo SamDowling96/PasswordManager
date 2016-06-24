e3e9be91d19dd9f2f930428aa2c6e7182bf3e59b
Luke Bluett <luke@localhost.localdomain>
2016-06-24 19:52:17 +0100

    modified:   src/passwordmanager/Database_Handler.py 	Added variables to the class, to store information

--- src/passwordmanager/Database_Handler.py
+++ src/passwordmanager/Database_Handler.py
@@ -4,6 +4,15 @@ import sqlite3
 
 class Database_Handler:
 
+	__sqlite_file = ''
+	__table_name = ''
+	__field_id = 'id'
+	__field_password = 'password'
+	__field_account = 'account'
+	__field_description = 'description'
+	__type_string = 'STRING'
+	__type_int = 'INTEGER'
+	
 	def __init__(self):
 		conn = sqlite3.connect(sqlite_file)
 		c = conn.cursor()
