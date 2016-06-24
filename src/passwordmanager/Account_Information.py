3a0972e3eb93b26a37bbb086d7dc0f724569b02a
Sam Dowling <sam.dowling@students.ittralee.ie>
2016-06-24 21:30:27 +0100

    Removed Memes
    Status: Meme-Free

--- src/passwordmanager/Account_Information.py
+++ src/passwordmanager/Account_Information.py
@@ -3,41 +3,14 @@
 
 class Account_Information:
 
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-  __password = ''
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-  __account = ''
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-  __description = ''
+   __password = ''
+   __account = ''
+   __description = ''
 
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-  def __init__(self, password, account, description):
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-     self.__password = password
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-     self.__account = account
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-     self.__description = description
+   def __init__(self, password, account, description):
+      self.__password = password
+      self.__account = account
+      self.__description = description
 
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-  def to_string(self):
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-# MEMES!!!!!!!!!!
-     return 'Password: ' + self.__password + '\nAccount: ' + self.__account + '\nDescription: ' + self.__description
+   def to_string(self):
+      return 'Password: ' + self.__password + '\nAccount: ' + self.__account + '\nDescription: ' + self.__description
